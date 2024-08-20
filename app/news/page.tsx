3110d95e2aea6114294b5244cf6847f813eac60a
Taisuke <taisuke4432@gmail.com>
2024-08-20 21:54:15 +0900

    サイト全体にrevalidateを設定する

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import Pagenation from "@/app/_components/Pagenation";
 import SearchFeild from "@/app/_components/SearchFeild";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
-export const revalidate = 60;
-
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
